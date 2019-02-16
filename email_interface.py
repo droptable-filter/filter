b66e64d49f9b0a07bc63f891f265d80195bae469
William <wahotaho@gmail.com>
2019-02-15 21:09:07 -0800

    interface fixed

--- email_interface.py
+++ email_interface.py
@@ -1,23 +1,29 @@
+ORG_EMAIL   = "@gmail.com"
+FROM_EMAIL  = "bobbytables2019" + ORG_EMAIL
+FROM_PWD    = "Xt55OyItnw8BuL9WCwt1"
+SMTP_SERVER = "imap.gmail.com"
+
 import smtplib
 import time
 import imaplib
 import email
 
-ORG_EMAIL   = "@gmail.com"
-FROM_EMAIL  = "bobbytables2019" + ORG_EMAIL
-FROM_PWD    = "Xt55OyItnw8BuL9WCwt1"
-SMTP_SERVER = "imap.gmail.com"
-SMTP_PORT   = 993
+# -------------------------------------------------
+#
+# Utility to read email from Gmail Using Python
+#
+# ------------------------------------------------
 
-def readmail():
+def read_email_from_gmail():
     try:
         mail = imaplib.IMAP4_SSL(SMTP_SERVER)
         mail.login(FROM_EMAIL,FROM_PWD)
-        mail.select('Inbox',readonly=True)
+        mail.select('inbox')
 
         type, data = mail.search(None, 'ALL')
         mail_ids = data[0]
 
+        id_list = mail_ids.split()
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
 
@@ -28,24 +34,23 @@ def readmail():
             for response_part in data:
                 if isinstance(response_part, tuple):
                     msg = email.message_from_string(response_part[1])
+                    email_subject = msg['subject']
+                    email_from = msg['from']
 
                     if (isSpam(msg)):
                         print("SPAM!")
-                    else
+                    else:
                         print("Not spam.")
-
-                    #email_subject = msg['subject']
-                    #email_from = msg['from']
-                    #print('From : ' + email_from)
-                    #print('Subject : ' + email_subject)
+                    #print ('From : ' + email_from + '\n')
+                    #print ('Subject : ' + email_subject + '\n')
                     #for part in msg.walk():
                         # each part is a either non-multipart, or another multipart message
                         # that contains further parts... Message is organized like a tree
-                        #if part.get_content_type() == 'text/plain':
-                            #print part.get_payload() # prints the raw text
+                    #    if part.get_content_type() == 'text/plain':
+                    #        print part.get_payload() # prints the raw text
                     #print('\n')
 
     except Exception, e:
         print str(e)
 
-readmail()
+read_email_from_gmail()
